18864da3869ab10a88157278f307f82788471158
EugeBr <eugebordenave@gmail.com>
2022-07-25 19:28:10 +0200

    Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom"
 import useAuth from "../../context/auth/useAuth"
 import "./Navbar.css"
 
-const Navbar = () => {
+/*const Navbar = () => {
 	// We are getting the user and some functions from the context
 	const { isLoggedIn, currentUser, removeUser } = useAuth()
 	// console.log(currentUser)
@@ -28,4 +28,102 @@ const Navbar = () => {
 	)
 }
 
-export default Navbar
+export default Navbar*/
+
+import { ReactNode } from 'react';
+import {
+	Box,
+	Flex,
+	Avatar,
+	Link,
+	Button,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	MenuDivider,
+	useDisclosure,
+	useColorModeValue,
+	Stack,
+	useColorMode,
+	Center,
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+
+/*const NavLink = ({ children }: { children: ReactNode }) => (
+  <Link
+	px={2}
+	py={1}
+	rounded={'md'}
+	_hover={{
+	  textDecoration: 'none',
+	  bg: useColorModeValue('gray.200', 'gray.700'),
+	}}
+	href={'#'}>
+	{children}
+  </Link>
+);*/
+
+export default function Navbar() {
+	const { isLoggedIn, currentUser, removeUser } = useAuth()
+	const { colorMode, toggleColorMode } = useColorMode();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	return (
+		<>
+			<Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+				<Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+					<NavLink to='/'>Logo</NavLink>
+					<NavLink to="/markets">All Markets</NavLink>
+					<Flex alignItems={'center'}>
+						<Stack direction={'row'} spacing={7}>
+							<Button onClick={toggleColorMode}>
+								{colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+							</Button>
+							{isLoggedIn && (
+								<>
+									<Menu>
+										<MenuButton
+											as={Button}
+											rounded={'full'}
+											variant={'link'}
+											cursor={'pointer'}
+											minW={0}>
+											<Avatar
+												size={'sm'}
+												src={currentUser.imgUrl}
+											/>
+										</MenuButton>
+										<MenuList alignItems={'center'}>
+											<br />
+											<Center>
+												<Avatar
+													size={'2xl'}
+													src={currentUser.imgUrl}
+												/>
+											</Center>
+											<br />
+											<Center>
+												<p>{currentUser.email}</p>
+											</Center>
+											<br />
+											<MenuDivider />
+											<MenuItem><NavLink to='/profile'>Your Profile</NavLink></MenuItem>
+											<MenuItem>Account Settings</MenuItem>
+											<MenuItem  onClick={removeUser}>Logout</MenuItem>
+										</MenuList>
+									</Menu>
+								</>
+							)}
+							{!isLoggedIn && (
+								<>
+									<NavLink to="/signin">Log in</NavLink>
+									<NavLink to="/signup">Sign Up</NavLink>
+								</>
+							)}
+						</Stack>
+					</Flex>
+				</Flex>
+			</Box>
+		</>
+	);
+}
