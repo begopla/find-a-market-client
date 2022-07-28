34fe287805944e92b1b6b395dd28add2ed11c11d
EugeBr <eugebordenave@gmail.com>
2022-07-28 22:07:20 +0200

    done

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,57 +1,54 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import useAuth from '../context/auth/useAuth';
-import { Flex, Image , Button} from '@chakra-ui/react';
-import ImageUploader from 'react-images-upload'
+import { Flex, Image, Button } from '@chakra-ui/react';
+//import ImageUploader from 'react-images-upload'
 import axios from 'axios';
 import { API_URL } from '../constants';
-import service  from '../services/apiHandler'
+import service from '../services/apiHandler'
 import { useNavigate } from 'react-router-dom';
 const EditProfile = () => {
-    const {currentUser, authenticateUser, storeToken} = useAuth();
-   
-    const [profilePicture, setProfilePicture] = useState([])
-    const navigate = useNavigate()
-    const onDrop = picture => {
+  const { currentUser, authenticateUser, storeToken } = useAuth();
 
-        setProfilePicture(picture)
-        console.log(picture[0])
+  const [profilePicture, setProfilePicture] = useState([])
+  const navigate = useNavigate()
+  const onDrop = picture => {
+
+    setProfilePicture(picture)
+    console.log(picture[0])
+  }
+  const submitPhoto = async (e) => {
+    e.preventDefault();
+    const fd = new FormData()
+    console.log(profilePicture)
+    if (profilePicture) {
+      fd.append("profilePicture", profilePicture)
+      console.log(fd)
+      try {
+        const data = await service.fileUpload(fd);
+        storeToken(data.token)
+        await authenticateUser()
+
+      } catch (error) {
+        console.error(error)
       }
-    const submitPhoto = async (e) =>{
-        e.preventDefault();
-        const fd = new FormData()
-        console.log(profilePicture)
-        if(profilePicture){
-        fd.append("profilePicture",profilePicture)
-        console.log(fd)
-            try {
-                console.log('start')
-               const data =  service.fileUpload(fd);
-               console.log('end')
-               console.log(data.message)
-                // storeToken(data.authToken)
-                // await authenticateUser()
-              // navigate('/')
-
-            } catch (error) {
-                console.error(error)
-            }
-        }
     }
-  
+  }
+
   return (
     <>
-     <Flex >
+      <Flex >
 
-        <Image 
-           boxSize='20vw'
-           ml='40vw'
-           borderRadius='full'
-           marginTop='5vh'
-           src={currentUser.profilePicture} 
-           alt={currentUser.name}
+        <Image
+          boxSize='20vw'
+          ml='40vw'
+          borderRadius='full'
+          marginTop='5vh'
+          objectFit='cover'
+          src={currentUser.profilePicture}
+          alt={currentUser.name}
         />
-     </Flex>
-     {/* <ImageUploader  withPreview={true}  
+      </Flex>
+      {/* <ImageUploader  withPreview={true}  
                         onChange={onDrop}
                         withIcon={true}
                         imgExtension={['.jpg', '.gif', '.png', '.gif']}
@@ -61,17 +58,17 @@ const EditProfile = () => {
      </div> */}
 
 
-     <form onSubmit={submitPhoto}>
-     <label htmlFor="profilePicture">Picture</label>
-				<input
-					type="file"
-					name="profilePicture"
-					multiple
-					onChange={(e) => setProfilePicture(e.target.files[0])}
-				/>
-            <Button type='submit' colorScheme='yellow' ml='40vw'>Upload</Button>
-     </form> 
-     </>
+      <form onSubmit={submitPhoto}>
+        <label htmlFor="profilePicture">Picture</label>
+        <input
+          type="file"
+          name="profilePicture"
+          multiple
+          onChange={(e) => setProfilePicture(e.target.files[0])}
+        />
+        <Button type='submit' colorScheme='yellow' ml='40vw'>Upload</Button>
+      </form>
+    </>
   )
 }
 
