98567d77a46d545a5d0bc8a1d426220d44695754
begopla <begonapr@gmail.com>
2022-08-18 13:52:36 +0400

    added display of users followed

---
+++ src/components/OneMarket/OneMarketSmall.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { Center, Box, Image, Badge, Skeleton } from '@chakra-ui/react';
+import { StarIcon } from '@chakra-ui/icons';
+
+function OneMarket({ name, _id, type, imageUrl, address }) {
+
+    return (
+        <Center>
+            <Link to={`/markets/${_id}`}>
+                <Box maxW='20vw' overflow='hidden' pt='4' mb='2'>
+                    <Image
+                        boxSize='20vw'
+                        objectFit='cover'
+                        src={imageUrl}
+                        alt={name}
+                        borderRadius='xl'
+                        boxShadow='lg' />
+                    <Box p='2'>
+                        <Box display='flex' alignItems='baseline' justifyContent='space-between'>
+                            <Badge borderRadius='full' px='2' colorScheme='teal'>
+                                {type}
+                            </Badge>   
+                        </Box>
+                        <Box
+                            mt='2'
+                            fontWeight='bold'
+                            fontSize='xl'
+                            as='h4'
+                            lineHeight='tight'
+                            noOfLines={1}
+                        >
+                            {name} 
+                        </Box>
+                    </Box>
+                </Box>
+            </Link>
+        </Center>
+    )
+}
+
+export default OneMarket
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,33 +1,90 @@
 import { useState, useEffect} from "react";
-import {Text, Box, Center, Button} from '@chakra-ui/react';
+import {Text, Box, Center, SimpleGrid, Grid, useColorModeValue, Avatar, Flex} from '@chakra-ui/react';
 import '../styles/profile.css';
 import DisplayMyMarket from "../components/DisplayMyMarket/DisplayMyMarket";
 import service from '../services/apiHandler';
+import OneMarketSmall from "../components/OneMarket/OneMarketSmall"
 const Profile = () => {
-	
+	const [followedUsers, setFollowedUsers] = useState([])
 	const [myMarkets, setMyMarkets] = useState([]);
 	const getMyMarkets = async () => {
 		const response = await service.myMarkets();
+		const res = await service.get('/profile/followed')
+		console.log(res.data.savedList)
 		setMyMarkets(response)
+		setFollowedUsers(res.data.savedList)
+
 	}
+	
 	useEffect(() => {
 		getMyMarkets();
 	}, []);
+	
 
 	return (
-		<Box height='95vh'>
-			<Text>PlaceHolder</Text>
+		<>
+		<Box height='90vh'>
+			
 			<Center><Text mt='8vh' fontSize='3xl'>My Markets</Text></Center>
+		{<SimpleGrid
+		bg={useColorModeValue('white', 'gray.700')}
+					minChildWidth='10rem'
+					spacing='5px'
+					className="listMarkets"
+					pb={'4rem'}
+					px={'3rem'}
+					pt={'3rem'}>
+		
+		
 		{myMarkets?.map((market) =>{
 			return(
-			<DisplayMyMarket 
-				{...market}
-				id={market._id}
-			/>
+				<OneMarketSmall
+								{...market}
+								key={market._id}
+								id={market._id}
+								name={market.name}
+							/>
 			)
 		})}
+
+		</SimpleGrid>}
 		
 		</Box>
+
+		<Box height='20vh'>
+			<Center ><Text  fontSize='3xl'>Users following</Text></Center>
+			<Grid
+			bg={useColorModeValue('white', 'gray.700')}
+					minChildWidth='5vw'
+					templateColumns={'repeat(5,1fr)'}
+					auto-fit
+					spacing='5px'
+					className="users"
+					pb={'2rem'}
+					px={'2rem'}
+					pt={'1rem'}
+					
+			>
+				{followedUsers?.map((user)=>{
+					return(
+						<Box width='20vw' marginLeft='10vw'>
+
+						<Flex direction='column'>
+
+						<Avatar
+							size='lg'
+							mt="0px"
+							src={user.profilePicture}
+							/>
+						<Text paddingLeft='2vw'>{user.name}</Text>
+						</Flex>
+						</Box>
+						
+					)
+				})}
+			</Grid>
+		</Box>
+		</>
 	)
 }
 
